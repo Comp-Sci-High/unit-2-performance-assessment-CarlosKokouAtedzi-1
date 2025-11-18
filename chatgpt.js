819e500676c68ff80a235926f8c12e1aa0a21f06
CarlosKokouAtedzi <kokou.atedzi26@compscihigh.org>
2025-11-18 16:54:44 -0500

    Update chatgpt.js

--- chatgpt.js
+++ chatgpt.js
@@ -5,3 +5,28 @@ const apiKey = process.env.apiKey1
 // run your code with node chatgpt.js
 // add your apiKey to .env from gChat
 // Follow the checklist on the Performance Assessment Google Doc
+let reqURL = "https://api.openai.com/v1/responses"
+qes = prompt("Input your request:")
+ 
+async function ChatAI() {
+
+const options = { method: 'POST',
+    headers: {
+      Authorization: 'Bearer ' + apiKey,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+        model:"gpt-4.1",
+        input: qes
+
+    }
+    )
+  };
+  const response = await fetch(reqURL, options);{
+
+  const data = await response.json()
+  console.log(data.output[0].content[0].text); 
+  return data;
+}
+}
+ChatAI()
