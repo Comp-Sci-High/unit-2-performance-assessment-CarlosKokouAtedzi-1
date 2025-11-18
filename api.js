0024f7fe5b920bcaa2f82f80c557e15e09d4a2f5
CarlosKokouAtedzi <kokou.atedzi26@compscihigh.org>
2025-11-18 16:54:14 -0500

    Update api.js

--- api.js
+++ api.js
@@ -5,3 +5,20 @@ const apiKey = process.env.apiKey2
 // run your code with node api.js
 // add your apiKey to .env if your API needs one, otherwise ignore
 // Follow the checklist on the Performance Assessment Google Doc
+let reqURL = "https://superheroapi.com/api/eec7960de7d2b65f2128689ab06a0b35"
+ 
+async function superhero(reqURL) {
+
+ const response = 
+ await fetch(reqURL);
+  
+    const data = await response.json()
+  console.log(data); 
+  return data;
+
+}
+let characterID = 1
+
+let stats = `${reqURL}/${characterID}/powerstats`;
+
+superhero(stats)
